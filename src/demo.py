7144cb608fae3f01c1a6e076c3e422dd09e01ad9
Mauro DiNuzzo <mauro.dinuzzo@hotmail.com>
2025-04-10 22:13:51 +0200

    updated demo.py to matlabengine 25.1

--- src/demo.py
+++ src/demo.py
@@ -1,25 +1,70 @@
 
-# Python3.9 (Python3.12 is required for MATLAB2025a)
-# $ export LD_LIBRARY_PATH=/usr/local/MATLAB/R2024b/bin/glnxa64/
-# $ python3 -m pip install matlabengine==24.2
+# Copyright 2020-2025 Netabolics SRL | Mauro DiNuzzo <mauro@netabolics.ai>
+
+
+# This script tests calling Netabolics MATLAB toolbox from Python.
+# Requirements: Python3.12 and MATLAB2025a
+#
+# Install matlabengine==25.1 
+#   $ cd /usr/local/MATLAB/R2025a/extern/engines/python
+#   $ sudo python3 setup.py install
+#
+# Launch
+#   $ cd /path-to/demo
+#   $ python3 -i demo.py
+
 
 import matlab.engine
 
-eng = matlab.engine.start_matlab()
+MATLAB = matlab.engine.start_matlab()
 
 # Move to the toolbox folder
 ToolboxPath = "/home/mauro/Netabolics/toolbox/matlab"
-eng.cd(ToolboxPath,nargout=0)
+MATLAB.cd(ToolboxPath,nargout=0)
 
-eng.init__()
+MATLAB.eval("init__()",nargout=0)
 
 # Move back to the demo folder
 DemoPath = "/home/mauro/Netabolics/demo"
-end.cd(DemoPath,nargout=0)
+MATLAB.cd(DemoPath,nargout=0)
 
-# Test loading model
-eng.eval("Cell = Netabolics.Biology.Model('../datasets/Cell.mat')",nargout=0)
-eng.eval("disp(Cell)",nargout=0)
+# Loading model
+cell = MATLAB.Netabolics.Biology.Model("datasets/Cell.mat")
 
-# Test passing object
-cell = eng.workspace["Cell"]
+# Display Python variable
+print(cell) 
+# <matlab.object object at 0x790f9684fef0> 
+
+# Copy object into MATLAB workspace
+MATLAB.workspace["Cell"] = cell 
+
+# Display Matlab variable
+MATLAB.eval("disp(Cell)",nargout=0)
+
+# Display the SBML property 
+MATLAB.eval("disp(Cell.SBML)",nargout=0)
+# This should output something like: 
+#              typecode: 'SBML_MODEL'
+#                metaid: '_NTBL_R20250303190144'
+#                 notes: ''
+#            annotation: ''
+#            SBML_level: 3
+#          SBML_version: 1
+#                  name: 'Human Genome-Scale Generic Biological Network'
+#                    id: 'NTBL_R20250303190144'
+#               sboTerm: -1
+#    functionDefinition: [1×0 struct]
+#        unitDefinition: [1×0 struct]
+#           compartment: [1×1 struct]
+#               species: [1×21754 struct]
+#             parameter: [1×0 struct]
+#     initialAssignment: [1×0 struct]
+#                  rule: [1×0 struct]
+#            constraint: [1×0 struct]
+#              reaction: [1×88643 struct]
+#                 event: [1×0 struct]
+#            namespaces: [1×0 struct]
+#                     S: [21754×88643 double]
+#               cvterms: [1×2 struct]
+#            identifier: [1×1 struct]
+
